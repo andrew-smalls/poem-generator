c090c38872d70a9285e94120b536e05be04e5104
Andrei Micu <andrei.micu@visma.com>
2023-04-01 00:33:20 +0300

    Initial commit

---
+++ src/main/java/com/example/itec/source/Controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package source.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import source.Model.Poem;
+import source.Service.PoemService;
+
+@RestController
+public class HelloWorldController {
+    @Autowired
+    PoemService poemService;
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello User";
+    }
+
+    @RequestMapping("/getPoem/{id}")
+    public String getPoem(@PathVariable int id) {
+        Poem poem = poemService.get(id);
+        String content = poem.getContent();
+        return "Fetched: " + content;
+    }
+}
