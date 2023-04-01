a46a32284590e7d01e3f6b61abaa8fd7e79a9b14
Andrei Micu <andrei.micu@visma.com>
2023-04-01 14:58:00 +0300

    Update project structure

--- src/App.js
+++ src/App.js
@@ -3,23 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      <>
+        <ChangingBackground>
+          <PageContent>
+          <InputForm/>
+          {basicReactApp()}
+          </PageContent>
+        </ChangingBackground>
+      </>
+  );
+}
+
+function ChangingBackground({children}) {
+  //logic to change background depending on input
+  return <>{children}</>;
+}
+
+function PageContent({children}) {
+  return <>{children}</>;
+}
+function InputForm() {
+  return <form>
+    <label>
+      Name:
+      <input type="text" name="name" />
+    </label>
+    <input type="submit" value="Submit" />
+  </form>
+}
+
+function basicReactApp() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn Reactsdfdas
-        </a>
-      </header>
-    </div>
-  );
+      >
+        Learn Reactsdfdas
+      </a>
+    </header>
+  </div>;
 }
+
 
 export default App;
