27116002411dcfde496fd71488f8d15544afbc04
Andrei Micu <andrei.micu394@gmail.com>
2025-09-05 11:20:21 +0200

    Project restructure (1.2)

---
+++ poem-generator-backend/src/main/java/poemgenerator/validations/ValidSize.java
@@ -0,0 +1,21 @@
+package itec.source.validations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import poemgenerator.validations.SizeValidator;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = SizeValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidSize {
+
+    String message() default "Invalid value. This is not a valid size.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
